3fcd8a7aac4e5c57749371b2addc1d68b0df5124
Dencik <agafonov.denis11@gmail.com>
2024-05-03 13:41:32 +0300

    Ship exercise corrected

--- src/SeaBigShip.java
+++ src/SeaBigShip.java
@@ -7,9 +7,9 @@ public class SeaBigShip {
             Scanner sc = new Scanner(System.in);
             System.out.print("Enter the position of big ship:");
             big_ship = sc.nextInt();
-        } while (big_ship < 1 || big_ship > 8);
+        } while (big_ship < 2 || big_ship > 9);
 
-        for(int x=0; x<10; x++) {
+        for(int x=1; x<11; x++) {
             if (x == big_ship) {
                 System.out.print( "W" );
             } else if (x == (big_ship-1)||x == (big_ship+1)) {
