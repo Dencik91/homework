8af2b28a3e51216295b134e5e41d83632577f2ed
Dencik <agafonov.denis11@gmail.com>
2024-05-10 14:16:24 +0300

    facuta clasa CoffeeMachine aparte

--- src/Student/homework/exercise/coffeeapp/Main.java
+++ src/Student/homework/exercise/coffeeapp/Main.java
@@ -1,9 +1,11 @@
 package Student.homework.exercise.coffeeapp;
 
-public class CoffeeApp {
+public class Main {
     public static void main(String[] args) {
+        CoffeeMachine homeCoffeeMachine = new CoffeeMachine();
         Container cup = new Container();
-
+        cup.setVolume(147);
+        homeCoffeeMachine.pour(cup);
     }
 }
 
