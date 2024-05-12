a154e88cb26cd56b279c4f9bd7595fd3437e1d07
Dencik <agafonov.denis11@gmail.com>
2024-05-12 21:05:30 +0300

    GitHub MoneyWireApp cu frumusata si test class

--- src/Student/homework/exercise/moneywireapp/MoneyWireApp.java
+++ src/Student/homework/exercise/moneywireapp/MoneyWireApp.java
@@ -5,7 +5,15 @@ public class MoneyWireApp {
         System.out.println("Bank account: "+Bank.money+"$");
         System.out.println("Juridic Person account: "+Bank.JuridicPersonClient.money+"$");
         System.out.println("Physical Person account: "+Bank.PhysicalPersonClient.money+"$");
-        Bank.transferFromJuridicToPhysical(1000);
+        Bank.transferFromJuridicToPhysical(5000);
+        Bank.transferFromJuridicToPhysical(5000);
+        Bank.transferFromJuridicToPhysical(5000);
+        Bank.transferFromJuridicToPhysical(5000);
+        Bank.transferFromJuridicToPhysical(5000);
+        Bank.transferFromJuridicToPhysical(5000);
+        Bank.transferFromJuridicToPhysical(5000);
+        Bank.transferFromJuridicToPhysical(4000);
+        Bank.transferFromJuridicToPhysical(1050);
         System.out.println("Bank account: "+Bank.money+"$");
         System.out.println("Juridic Person account: "+Bank.JuridicPersonClient.money+"$");
         System.out.println("Physical Person account: "+Bank.PhysicalPersonClient.money+"$");
@@ -38,14 +46,19 @@ class Bank {
             money += 10;
             System.out.println("Successfully transfer 10$ to Bank account");
         } else {
-            int bankCommission = amount / 100 + amount % 100;
+            int bankCommission = 0;
+            if (amount % 100 != 0) {
+                bankCommission = amount / 100 + 1;
+            } else {
+                bankCommission = amount / 100;
+            }
             JuridicPersonClient.money -= amount;
             System.out.println("Successfully withdraw "+amount+"$ from Juridic Person");
             PhysicalPersonClient.money += (amount-bankCommission);
             System.out.println("Successfully transfer "+(amount-bankCommission)+
                     "$ to Physical Person");
             money += bankCommission;
-            System.out.println("Successfully transfer"+bankCommission+"$ to Bank account");
+            System.out.println("Successfully transfer "+bankCommission+"$ to Bank account");
         }
     }
 
